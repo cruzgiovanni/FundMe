76ab5d6a8e0b65c3c5e8c1e6570b252436d1f25c
cruzgiovanni <giovannicruzvinhedo@gmail.com>
2025-09-02 20:43:10 -0300

    update: FundMe contract update to latest version (gas saver improvements, special functions)

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -3,19 +3,59 @@ pragma solidity ^0.8.30;
 
 import "./PriceConverter.sol";
 
+error NotOwner();
+
 contract FundMe {
   using PriceConverter for uint256;
 
-  uint256 public minimunUsd = 50 * 1e18;
+  uint256 public constant MINIMUN_USD = 50 * 1e18;
 
   address[] public funders;
   mapping(address => uint256) public addressToAmountFunded;
 
-  function fund() public payable {
-    require(msg.value.getConversionRate() >= minimunUsd, "Didn't send enough!");
-    funders.push(msg.sender);
-    addressToAmountFunded[msg.sender] = msg.value;
+  address public immutable i_owner;
+
+  constructor() {
+    i_owner = msg.sender;
   }
 
-  // function withdraw() public {}
+  function fund() public payable {
+    require(
+      msg.value.getConversionRate() >= MINIMUN_USD,
+      "Didn't send enough!"
+    );
+    funders.push(msg.sender);
+    addressToAmountFunded[msg.sender] += msg.value;
+  }
+
+  modifier onlyOwner() {
+    // require(msg.sender == i_owner, "Sender is not owner!");
+    // Gas saver
+    if (msg.sender != i_owner) {
+      revert NotOwner();
+    }
+    _;
+  }
+
+  function withdraw() public onlyOwner {
+    for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+      address funder = funders[funderIndex];
+      addressToAmountFunded[funder] = 0;
+    }
+    // reset the array
+    funders = new address[](0);
+
+    (bool callSuccess, ) = payable(msg.sender).call{
+      value: address(this).balance
+    }("");
+    require(callSuccess, "Call failed");
+  }
+
+  receive() external payable {
+    fund();
+  }
+
+  fallback() external payable {
+    fund();
+  }
 }
